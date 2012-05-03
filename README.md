1217e73824c979b83fdb35666dbc6d910f224a66
Brendan Spinks <bspinks@dius.com.au>
2012-05-03 10:11:04 +1000

    Initial import

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-coffeematrix
-============
+# ReadMe
 
-built using appscrolls
+
+## Deployment
+
+```
+ey deploy
+```
+Remove
+## Thanks
+
+The original scaffold for this application was created by [App Scrolls](http://appscrolls.org).
+
+The project was created with the following scrolls:
+
+* capybara
+* rails_basics
+* git
+* github
+* jquery
+* mysql
+* rspec
+* twitter_bootstrap
+* simple_form
+* unicorn
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffeematrix::Application.config.secret_token = '14e6773e0bdfa37a3d8b4d0e36d7f19cb41b2e177b86e646fbf4522c2876c333b99a98b36b44f8607572e01c31d24665d20accf75823625ead9a8f0f623d2bff'
